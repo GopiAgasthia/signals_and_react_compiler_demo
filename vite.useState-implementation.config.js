ead2257326481560d1c5abd7862e9d2d0ce5c747
GopiAgasthia <69740917+GopiAgasthia@users.noreply.github.com>
2025-08-08 19:55:52 +0530

    Update vite.useState-implementation.config.js

--- vite.useState-implementation.config.js
+++ vite.useState-implementation.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(
-        {
-            babel: {
-                plugins: ['babel-plugin-react-compiler']
-            }
-        }
+        // {
+        //     babel: {
+        //         plugins: ['babel-plugin-react-compiler']
+        //     }
+        // }
     )],
     root: 'workspace-entries/useState-implementation',
     publicDir: 'public',
